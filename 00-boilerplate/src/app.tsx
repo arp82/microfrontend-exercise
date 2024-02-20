6e451e502acd187cce34091ac18dc3a119b0ec1a
Alejandro Rosa Pujazon <alejandro.rosa.pujazon@ericsson.com>
2024-02-20 22:42:33 +0100

    Upload boilerplate

---
+++ 00-boilerplate/src/app.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const App = () => {
+  return <h1>Ericsson Webpack Module Federation Playground</h1>;
+};
---
+++ 00-boilerplate/webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const basePath = __dirname;
+
+module.exports = {
+  context: path.join(basePath, "src"),
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  entry: {
+    app: ["./index.tsx"],
+  },
+  devtool: "eval-source-map",
+  stats: "errors-only",
+  output: {
+    filename: "[name].[chunkhash].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "index.html",
+    }),
+  ],
+};
